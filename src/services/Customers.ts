6ad3b8846b46a2590918b0943a2a765cebe5cb1b
Kyrylo Usichenko <kyrylo.usichenko@gmail.com>
2023-06-22 01:21:04 +0300

    add: do customers benchmarks and sql query as string

---
+++ src/services/Customers.ts
@@ -0,0 +1,36 @@
+import { CustomersRepository } from '@/database/repositories/Customers';
+
+export class CustomersService {
+	constructor(private customers: CustomersRepository) {}
+	public getAll = (start: number, count: number) => {
+		const startTimestamp = Date.now();
+		const customers = this.customers.getAll(start, count);
+		const endTimestamp = Date.now();
+		const time = endTimestamp - startTimestamp;
+
+		const query = this.customers.getAllQuery(start, count);
+		query.params.forEach((param: any) => {
+			query.sql = query.sql.replace('?', param);
+		});
+		query.sql = query.sql.replace('\\', ' ');
+		return {
+			sql: query.sql,
+			customers,
+			time,
+		};
+	};
+	public getOne = (id: string) => {
+		const startTimestamp = Date.now();
+		const customer = this.customers.getOne(id);
+		const endTimestamp = Date.now();
+		const time = endTimestamp - startTimestamp;
+
+		const query = this.customers.getOneQuery(id);
+		query.params.forEach((param: any) => {
+			query.sql = query.sql.replace('?', param);
+		});
+		query.sql = query.sql.replace('\\', ' ');
+
+		return { customer, time, sql: query.sql };
+	};
+}
